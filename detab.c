5a9ef319d8e1d8b1cba49bc8cdf13e0b17e71b6e
Mholder1 <marcus.holder@gmail.com>
2023-09-13 17:31:53 +0100

    Minor syntax change.

--- detab.c
+++ detab.c
@@ -34,10 +34,10 @@ int getline(void)
     for (i=0; (c = getchar()) != EOF && c != '\n'; ++i)
         line[i] = c;
     if (c == '\n')
-        {
-            line[i] = c;
-            ++i;
-        }
+    {
+        line[i] = c;
+        ++i;
+    }
     line[i] = '\0';
     return i;
 }
